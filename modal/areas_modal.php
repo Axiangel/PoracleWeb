9e04549944dfec6f6cec76eb445a228e03e03022
Axi <49078493+Axiangel@users.noreply.github.com>
2025-10-17 11:12:19 +0200

    Update areas_modal.php
    Using Multibyte

--- modal/areas_modal.php
+++ modal/areas_modal.php
@@ -45,7 +45,7 @@ if(count(array_keys($areas)) === 1){
         echo "<li><input type='checkbox' name='area_$area_var' id='area_$area_var' $checked/>\n";
 	echo "<label for='area_$area_var' style='width:160px;'><font style='font-size:12px;'>$area</font>\n";
 	if (@$disable_geomap_select <> "True") {
-		$area = mb_strtoupper(str_replace(' ', '_', $area));
+		$area = mb_mb_strtoupper(str_replace(' ', '_', $area));
 		echo "<br><img src='.cache/geo_".$area."_".$hash.".png' style='width:100%; max-width=100%;'>";
 	}
 	echo "</label>\n";
@@ -85,7 +85,7 @@ if(count(array_keys($areas)) === 1){
             echo "<li><input type='checkbox' name='area_$area_var' id='area_$area_var' $checked/>\n";
 	    echo "<label for='area_$area_var' style='width:160px;'><font style='font-size:12px;'>$area</font>\n";
 	    if (@$disable_geomap_select <> "True") {
-		    $area = mb_strtoupper(str_replace(' ', '_', $area));
+		    $area = mb_mb_strtoupper(str_replace(' ', '_', $area));
                     echo "<br><img src='.cache/geo_".$area."_".$hash.".png' style='width:100%; max-width=100%;'>";
             }
 	    echo "</label>\n";
